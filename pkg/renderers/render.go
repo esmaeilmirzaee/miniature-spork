421514996603868ffcea9a45361d5802151a4f56
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-11-30 19:35:44 +0330

    Use template and layout

--- pkg/renderers/render.go
+++ pkg/renderers/render.go
@@ -1,16 +1,71 @@
 package renderers
 
 import (
-	"github.com/esmaeilmirzaee/random-time-sleeper/helpers"
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions template.FuncMap
+
 func RenderTemplate(w http.ResponseWriter, templateName string) {
-	templateFile, err := template.ParseFiles("./templates/"+templateName)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		helpers.HandleErrors(err)
-		return
+		log.Fatal(err)
 	}
-	err = templateFile.Execute(w, nil)
+
+	t, ok := tc[templateName]
+	if !ok {
+		log.Fatal("Can't read template")
+	}
+
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println("Error executing template", err)
+	}
+
+	_, err = buf.WriteTo(w)
+
+	if err != nil {
+		log.Println("Error writing template to browser", err)
+	}
+}
+
+
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
